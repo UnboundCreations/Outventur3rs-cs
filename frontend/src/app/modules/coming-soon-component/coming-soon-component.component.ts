46cfe846bc052034c0d582095a56cd6763de6f40
miguelrodriguez6 <miguel.rodriguez6@udc.es>
2024-10-17 23:40:54 +0200

    feat: coming-soon component

--- frontend/src/app/modules/coming-soon-component/coming-soon-component.component.ts
+++ frontend/src/app/modules/coming-soon-component/coming-soon-component.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-coming-soon-component',
   templateUrl: './coming-soon-component.component.html',
   styleUrl: './coming-soon-component.component.scss'
 })
-export class ComingSoonComponentComponent {
+export class ComingSoonComponentComponent implements OnInit, OnDestroy {
+
+  moreInfo: boolean = false;
+  countdown: number = 59;
+  intervalId: any;
+
+  ngOnInit() {
+    this.startCountdown();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  startCountdown() {
+    this.intervalId = setInterval(() => {
+      this.countdown--;
+      if (this.countdown < 0) {
+        this.countdown = 59;
+      }
+    }, 1000);
+  }
 
 }
